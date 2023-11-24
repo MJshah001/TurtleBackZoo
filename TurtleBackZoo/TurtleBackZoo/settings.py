b623b1abf0bb59b0fb4cce82832cde3ddc9b5048
Monil Shah <monilshah1984@gmail.com>
2023-11-24 17:53:42 -0500

    managementandreporting setup

--- TurtleBackZoo/TurtleBackZoo/settings.py
+++ TurtleBackZoo/TurtleBackZoo/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
     'DailyZooAcitvity.apps.DailyzooacitvityConfig',
+    'ManagementAndReporting.apps.ManagementandreportingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
